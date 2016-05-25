7de6cab0d5912d1bae6ced5cf1b70c09178e499b
dufresnedavid <david.dufresne@savoirfairelinux.com>
2016-05-25 17:08:00 -0400

    Debug report_webkit

--- addons/report_webkit/webkit_report.py
+++ addons/report_webkit/webkit_report.py
@@ -205,6 +205,7 @@ class WebKitParser(report_sxw):
         stderr_fd, stderr_path = tempfile.mkstemp(text=True)
         file_to_del.append(stderr_path)
         try:
+            print command
             status = subprocess.call(command, stderr=stderr_fd)
             os.close(stderr_fd) # ensure flush before reading
             stderr_fd = None # avoid closing again in finally block
